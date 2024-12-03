216d51de28f19b9c20cca3bfc6d02a612babe6c4
Jinad Tope Isaac <jinadtope66@gmailcom>
2024-12-03 15:24:50 +0100

    updated the outreach page

--- app/Outreach/page.jsx
+++ app/Outreach/page.jsx
@@ -39,7 +39,7 @@ export default function OutreachPage() {
           />
           <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center">
             <h1 className="text-white text-4xl md:text-5xl font-bold">
-              Latest Sermons
+              Outreach
             </h1>
           </div>
         </section>
